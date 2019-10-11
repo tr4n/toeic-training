e58e3f0f490084ed9c7b88c6a437c21efd362ffb
huytq-0932 <tran.quang.huy@sun-asterisk.com>
2019-10-11 11:00:45 +0700

    temp

---
+++ app/src/main/java/com/example/toeictraining/base/BaseActivity.kt
@@ -0,0 +1,27 @@
+package com.example.toeictraining.base
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    protected abstract val layoutResource: Int
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        supportActionBar?.hide()
+        super.onCreate(savedInstanceState)
+        setContentView(layoutResource)
+        initComponent()
+        initData()
+    }
+
+    protected abstract fun initComponent()
+
+    protected abstract fun initData()
+
+    protected fun openFragment(id: Int, fragment: Fragment, addToBackStack: Boolean) =
+        supportFragmentManager.beginTransaction().replace(id, fragment).apply {
+            if (addToBackStack) addToBackStack(null)
+        }.commit()
+}
