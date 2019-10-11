79fd92ed4898c69c1640b12ed0701a940239b572
huytq-0932 <tran.quang.huy@sun-asterisk.com>
2019-10-11 11:07:30 +0700

    init base activity, fragment

--- app/src/main/java/com/example/toeictraining/MainActivity.kt
+++ app/src/main/java/com/example/toeictraining/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.example.toeictraining
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import com.example.toeictraining.base.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+    override val layoutResource: Int = R.layout.activity_main
+
+    override fun initComponent() {}
+
+    override fun initData() {}
 }
