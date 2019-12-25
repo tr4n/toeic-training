de64990812afdce4d9f2202084876a80d9ab9d19
Bui Trung Hieu <bui.trung.hieu@mqsolutions.com.vn>
2019-12-25 11:22:58 +0700

    Update Score View

--- app/src/main/java/com/example/toeictraining/ui/fragments/intro/IntroScoreFragment.kt
+++ app/src/main/java/com/example/toeictraining/ui/fragments/intro/IntroScoreFragment.kt
@@ -6,16 +6,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 import com.example.toeictraining.R
+import kotlinx.android.synthetic.main.intro_score_fragment.*
+import kotlinx.android.synthetic.main.navigation_view_header.*
 
 class IntroScoreFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = IntroScoreFragment()
-    }
-
     private lateinit var viewModel: IntroScoreViewModel
+
+    var spinner: Spinner? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,7 +29,20 @@ class IntroScoreFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(IntroScoreViewModel::class.java)
-        // TODO: Use the ViewModel
+
+        spinnerSelect()
     }
 
+    private fun spinnerSelect() {
+        val scores = resources.getStringArray(R.array.Scores)
+        spinner = spnIntensity
+        if (spinner != null) {
+            val adapter = ArrayAdapter(
+                context!!,
+                R.layout.intensity_spinner_item, scores
+            )
+            adapter.setDropDownViewResource(R.layout.intensity_spinner_dropdown_item)
+            spinner!!.adapter = adapter
+        }
+    }
 }
